615c1065f97cc4d52eaf557da7b5417adaf2faf3
BramKr <bramkreuger@gmail.com>
2022-10-04 17:03:11 +0200

    added fetch homeposts

---
+++ src/store/reducers/users.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: 'users',
+    initialState:{
+        action:{}        
+    },
+    reducers:{
+
+    }
+})
+
+export default userSlice.reducer;
